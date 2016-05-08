b7be21136aceb513ef280d66df6af798e82714f0
Kevin Smith <ksmithbaylor@gmail.com>
2016-05-08 18:08:21 -0400

    Find the latest budget file from a ynab directory

--- lib/ynab_analytics.ex
+++ lib/ynab_analytics.ex
@@ -1,2 +1,15 @@
 defmodule YnabAnalytics do
+  def latest_budget_file(ynab_dir) do
+    ynab_dir |> all_budget_files |> most_recent_file
+  end
+
+  #####
+
+  defp all_budget_files(ynab_dir) do
+    Path.wildcard(ynab_dir <> "/**/*.yfull")
+  end
+
+  defp most_recent_file(paths) do
+    Enum.max_by(paths, fn path -> File.stat!(path).mtime end)
+  end
 end
