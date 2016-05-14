f2803023984e457e2c7d498328791124f5cb92fc
Kevin Smith <ksmithbaylor@gmail.com>
2016-05-14 16:09:29 -0500

    Add date function to Transaction

--- lib/ynab/transaction.ex
+++ lib/ynab/transaction.ex
@@ -1,4 +1,6 @@
 defmodule YNAB.Transaction do
+  alias YNAB.Dates
+
   @derive [Poison.Encoder]
   defstruct [
     :accepted,
@@ -16,5 +18,10 @@ defmodule YNAB.Transaction do
 
   def valid?(transaction) do
     !transaction.isTombstone
+  end
+
+  def date(transaction) do
+    Dates.parse_date(transaction.date)
+    |> :calendar.gregorian_days_to_date
   end
 end
