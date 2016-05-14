100d616fce1ca437f652fd7081018599c1d1b73a
Kevin Smith <ksmithbaylor@gmail.com>
2016-05-13 22:31:38 -0500

    Refactor Transactions for better functional style

--- lib/ynab/transactions.ex
+++ lib/ynab/transactions.ex
@@ -2,31 +2,34 @@ defmodule YNAB.Transactions do
   alias YNAB.Account
   alias YNAB.Transaction
 
-  def only_valid(transactions) do
-    transactions
-    |> Enum.filter(fn t -> Transaction.valid?(t) end)
+  def for_accounts(accounts, transactions) do
+    accounts
+    |> Enum.flat_map(fn account ->
+      for_account(transactions, account)
+    end)
   end
 
   def for_account(transactions, %Account{entityId: accountId}) do
-    only_valid(transactions)
-    |> Enum.filter(fn t -> t.accountId == accountId end)
+    only_valid(transactions) |> with_account_id(accountId)
   end
 
-  def for_accounts(accounts, transactions) do
-    accounts
-    |> Enum.flat_map(fn account -> for_account(transactions, account) end)
+  def only_valid(transactions) do
+    transactions |> Enum.filter(& Transaction.valid?(&1))
+  end
+
+  def with_account_id(transactions, accountId) do
+    transactions |> Enum.filter(& &1.accountId == accountId)
   end
 
   def sum(transactions) do
     transactions
-    |> Enum.map(fn t -> t.amount end)
+    |> Enum.map(& &1.amount)
     |> currency_sum
   end
 
   defp currency_sum(amounts) do
     bigSum = amounts
-             |> Enum.map(fn amount -> amount * 100 end)
-             |> Enum.map(&round/1)
+             |> Enum.map(& round(&1 * 100))
              |> Enum.sum
     bigSum / 100
   end
