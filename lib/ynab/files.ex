fcd73062c2fd136d95213f2f5db87e5a8a3ce975
Kevin Smith <ksmithbaylor@gmail.com>
2016-05-13 22:05:18 -0500

    Reformatting

--- lib/ynab/files.ex
+++ lib/ynab/files.ex
@@ -1,6 +1,8 @@
 defmodule YNAB.Files do
   def latest_budget_file(ynab_dir) do
-    ynab_dir |> all_budget_files |> most_recent_file
+    ynab_dir
+    |> all_budget_files
+    |> most_recent_file
   end
 
   defp all_budget_files(ynab_dir) do
@@ -8,6 +10,7 @@ defmodule YNAB.Files do
   end
 
   defp most_recent_file(paths) do
-    paths |> Enum.max_by(fn path -> File.stat!(path).mtime end)
+    paths
+    |> Enum.max_by(fn path -> File.stat!(path).mtime end)
   end
 end
