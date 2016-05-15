be1e8a6b0a573adca04ec00ec4124eaa34b37425
Kevin Smith <ksmithbaylor@gmail.com>
2016-05-14 23:28:35 -0500

    Remove gen_server from db

--- lib/ynab/db.ex
+++ lib/ynab/db.ex
@@ -1,32 +1,18 @@
 defmodule YNAB.DB do
   use GenServer
-  alias YNAB.Transaction
-  alias YNAB.Account
 
   @table Application.get_env(:ynab, :ets_table)
 
-  def start_link do
-    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
-  end
-
-  def insert(entity) do
-    GenServer.cast(__MODULE__, {:insert, entity})
-  end
-
-  # Callbacks
-
-  def init(:ok) do
+  def init do
     case :ets.info(@table) do
       :undefined ->
         :ets.new(@table, [:set, :public, :named_table])
       _ ->
         :ets.delete_all_objects(@table)
     end
-    {:ok, @table}
   end
 
-  def handle_cast({:insert, entity}, _table) do
+  def insert(entity) do
     :ets.insert(@table, {entity.entityId, entity.entityType, entity})
-    {:noreply, @table}
   end
 end
