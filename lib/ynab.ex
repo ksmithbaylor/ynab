64eaa8215e4a9b8269a100d22d05697f77723fcf
Kevin Smith <ksmithbaylor@gmail.com>
2016-05-15 14:00:05 -0500

    Non-working ets implementation

--- lib/ynab.ex
+++ lib/ynab.ex
@@ -1,7 +1,26 @@
 defmodule YNAB do
   alias YNAB.Budget
 
-  def budget_for(ynab_dir) do
-    ynab_dir |> Budget.from_budget_dir
+  @table Application.get_env(:ynab, :ets_table)
+
+  def init(ynab_dir) do
+    init_ets
+    budget = ynab_dir |> Budget.from_budget_dir
+    budget.transactions |> Enum.map(&insert_entity/1)
+    budget.accounts |> Enum.map(&insert_entity/1)
+    budget
+  end
+
+  defp init_ets do
+    case :ets.info(@table) do
+      :undefined ->
+        :ets.new(@table, [:set, :public, :named_table])
+      _ ->
+        :ets.delete_all_objects(@table)
+    end
+  end
+
+  defp insert_entity(entity) do
+    :ets.insert(@table, {entity.entityId, entity.entityType, entity})
   end
 end
